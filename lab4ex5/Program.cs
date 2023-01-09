d16ed643277ed0f0ed0fdb7c13368171a6414928
madalina.toma <madalina.toma@softem.ro>
2023-01-09 11:52:39 +0200

    Add project files.

---
+++ lab4ex5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab4ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 5
+             Scrieti o functie recursiva care va calcula suma numerelor de la 1 
+             pana la n, apelati-o si afisati-i rezultatul
+
+             */
+            Console.Write("Introduceti valoare lui n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Suma numerelor este: " + SumNumbers(n));
+        }
+
+        static int SumNumbers(int n)
+        {
+            var sum = 0;
+          
+            if(n != 0)
+            {
+                sum= n + SumNumbers(n - 1);
+                return sum;
+            }
+            return sum;
+        }
+    }
+}
